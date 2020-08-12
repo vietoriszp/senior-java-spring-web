be4f3cc6b80c79c8f91cd1d42c53fd774f00dc9b
vpeter <peter.vietorisz@gmail.com>
2020-08-13 00:02:00 +0200

    Konstrukror publikussá tétele.

--- src/main/java/hu/ponte/hr/controller/ImageMeta.java
+++ src/main/java/hu/ponte/hr/controller/ImageMeta.java
@@ -1,18 +1,24 @@
 package hu.ponte.hr.controller;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 /**
+ * A megjelenítendő kép metaadatokat tartalmató osztály.
+ *
  * @author zoltan
+ * @author vpeter
  */
 @Getter
 @Builder
-public class ImageMeta
-{
-	private String id;
-	private String name;
-	private String mimeType;
-	private long size;
-	private String digitalSign;
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class ImageMeta {
+    private String id;
+    private String name;
+    private String mimeType;
+    private long size;
+    private String digitalSign;
+
 }
